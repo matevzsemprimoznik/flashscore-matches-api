f95bd5db3bb2340ae96a6cc3faec1d93f3e8e6e1
Matevz <semprimoznik.matevz@gmail.com>
2024-03-07 13:20:56 +0100

    project initialization

---
+++ src/main/java/com/flashscore/flashscorematchesapi/FlashscoreMatchesApiApplication.java
@@ -0,0 +1,13 @@
+package com.flashscore.flashscorematchesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashscoreMatchesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashscoreMatchesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashscore/flashscorematchesapi/FlashscoreMatchesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashscore.flashscorematchesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashscoreMatchesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
